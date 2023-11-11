3af97e64ec441935193bfb7bd394cd4f4455e398
Nathan Q <nathanq@NatesHomeBuilt>
2023-11-11 12:07:46 -0500

    proj setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AprilTagPoseEstimation
+
+# Dependencies
+- opencv https://opencv.org/
+- apriltags https://github.com/AprilRobotics/apriltag
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <apriltag/apriltag.h>
+
+
+int main()
+{
+    cv::VideoCapture cap = cv::VideoCapture(0);
+
+    return 0;
+}
